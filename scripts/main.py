0c7b7bd4c204013bde731eb639722d1e41131450
Stephen Welch <stephenwelchva@vt.edu>
2025-04-28 22:15:40 -0400

    WIP QP

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,6 @@
 import mujoco
 import mujoco.viewer
-import casadi
+import casadi as ca
 import time
 import numpy as np
         
@@ -8,8 +8,29 @@ from casadi import vec
 
 np.set_printoptions(precision=3, linewidth=1000)
 
-def main():
+def skew(v):
+    return np.array([
+        [0, -v[2], v[1]],
+        [v[2], 0, -v[0]],
+        [-v[1], v[0], 0],
+    ])
 
+def quat_to_euler(q):
+    return np.array([
+        np.arctan2(2*(q[0]*q[1] + q[2]*q[3]), 1 - 2*(q[1]**2 + q[2]**2)),
+        np.arcsin(2*(q[0]*q[2] - q[3]*q[1])),
+        np.arctan2(2*(q[0]*q[3] + q[1]*q[2]), 1 - 2*(q[2]**2 + q[3]**2)),
+    ])
+
+def get_R(model: mujoco.MjModel, data: mujoco.MjData):
+    roll, pitch, yaw = quat_to_euler(data.qpos[3:7])
+    return np.array([
+        [np.cos(yaw), -np.sin(yaw), 0],
+        [np.sin(yaw), np.cos(yaw), 0],
+        [0, 0, 1],
+    ])
+
+def main():
     
     model = mujoco.MjModel.from_xml_path("assets/unitree_go1/scene.xml")
     data = mujoco.MjData(model)
@@ -20,6 +41,10 @@ def main():
     LEG_NAMES = ["FR", "FL", "RR", "RL"]
     FEET_SITE_NAMES = {
         name: name 
+        for name in LEG_NAMES
+    }
+    FEET_CONTACT_SENSOR_NAMES = {
+        name: f"{name}_contact"
         for name in LEG_NAMES
     }
     LEG_JNT_IDXS = {
@@ -42,7 +67,7 @@ def main():
 
     print(model.body("trunk").subtreemass)
 
-    opti = casadi.Opti()
+    opti = ca.Opti()
 
     PENALTY_L1_GRF = 0.0
     PENALTY_L2_GRF_SLEW = 0.0
@@ -59,8 +84,63 @@ def main():
     opti.subject_to(vec(F) >= F_MIN)
     opti.subject_to(vec(F) <= F_MAX)
 
-    def update_opt(model: mujoco.MjModel, data: mujoco.MjData):
-        ...
+    def update_opt(model: mujoco.MjModel, data: mujoco.MjData, sol_last: ca.OptiSol | None = None):
+        m = model.body("trunk").mass
+        g = np.array([0, 0, -9.81]).reshape(3, 1)
+        I_G = data.body("trunk").ximat.reshape(3, 3)
+
+        R = data.body("trunk").xmat.reshape(3, 3)
+        theta = quat_to_euler(data.qpos[3:7])
+        p_c = data.body("trunk").xipos
+        p_i = np.array([data.site(FEET_SITE_NAMES[n]).xpos for n in FEET_SITE_NAMES])
+        p_c_dot = data.body("trunk").cvel[:3]
+        w_b = data.body("trunk").cvel[3:]
+        # p_c_ddot = data.body("trunk").cacc[:3, None]
+        # w_b_ddot = R.T @ data.body("trunk").cacc[3:, None]
+
+        M = np.block([
+            [m*np.eye(3), np.zeros((3, 3))],
+            [np.zeros((3, 3)), I_G],
+        ])
+        G = np.block([
+            [g],
+            [np.zeros((3, 1))],
+        ])
+        A_ = np.block([
+            [np.eye(3) for _ in range(N_FEET)],
+            [skew(p_i[i] - p_c) for i in range(N_FEET)] # TODO vectorize
+        ])
+
+        # b_ = M @ np.block([
+        #     [p_c_ddot],
+        #     [w_b_ddot],
+        # ]) + G
+        K_P = np.eye(6)
+        K_D = np.eye(6)
+        x_des = np.array([
+            0, 0, 0.445, # p_c
+            0, 0, 0, # theta
+            0, 0, 0, # p_c_dot
+            0, 0, 0, # w_dot
+        ])
+        x = np.block([p_c, theta, p_c_dot, w_b])
+        K = np.block([-K_P, -K_D])
+        u = K @ (x_des - x)
+        b_ = M @ (u.reshape(6, 1) + G)
+
+        C_ = np.eye(N_FEET)
+        S_ = np.eye(6)
+        if sol_last is not None:
+            F_last_ = sol_last.value(F_last)
+        else:
+            F_last_ = np.zeros((N_FEET, 3))
+
+        print(A_.shape)
+        opti.set_value(A, ca.DM(A_.tolist()))
+        opti.set_value(b, ca.DM(b_.tolist()))
+        opti.set_value(C, ca.DM(C_.tolist()))
+        opti.set_value(S, ca.DM(S_.tolist()))
+        opti.set_value(F_last, ca.DM(F_last_.tolist()))
 
     def ctrl_loop(model: mujoco.MjModel, data: mujoco.MjData, F: np.ndarray):
 
@@ -85,19 +165,25 @@ def main():
         init_pos = np.array([model.site(FEET_SITE_NAMES[n]).pos for n in LEG_NAMES])
         print(init_pos)
 
+        sol = None
+        opti.solver("ipopt")
+
         start = time.time()
         while viewer.is_running():
             step_start = time.time()
 
-            feet_err = init_pos - np.array([model.site(FEET_SITE_NAMES[n]).pos for n in LEG_NAMES])
+            # feet_err = init_pos - np.array([model.site(FEET_SITE_NAMES[n]).pos for n in LEG_NAMES])
             
 
-            F_des = np.array([
-                [0, 0, 1],
-                [0, 0, 1],
-                [0, 0, 1],
-                [0, 0, 1],
-            ]) * -100
+            update_opt(model, data, sol)
+            sol = opti.solve()
+            F_des = sol.value(F)
+            # F_des = np.array([
+            #     [0, 0, 1],
+            #     [0, 0, 1],
+            #     [0, 0, 1],
+            #     [0, 0, 1],
+            # ]) * -100
             ctrl_loop(model, data, F_des)
             mujoco.mj_step(model, data)
 
--- scripts/view_model.py
+++ scripts/view_model.py
@@ -5,4 +5,10 @@ model = mujoco.MjModel.from_xml_path("assets/unitree_go1/scene.xml")
 
 
 data = mujoco.MjData(model)
-mujoco.viewer.launch(model, data)
+with mujoco.viewer.launch_passive(model, data) as viewer:
+    print(data.body("trunk"))
+    print(data.site("FR"))
+    print(data.qpos[:7])
+    while viewer.is_running():
+        mujoco.mj_step(model, data)
+        viewer.sync()
